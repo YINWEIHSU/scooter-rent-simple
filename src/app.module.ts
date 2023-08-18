8305836c5a6e601c6db17292fe774128adda1dfb
yinwei <cba5530@gmail.com>
2023-08-19 02:21:17 +0800

    generate modules, controllers, services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ScootersModule } from './scooters/scooters.module';
+import { RentalsModule } from './rentals/rentals.module';
+import { AdminsModule } from './admins/admins.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ScootersModule, RentalsModule, AdminsModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/scooters/scooters.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('scooters')
+export class ScootersController {}
